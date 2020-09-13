48034c87089797dd8cb96e7b05fb06bef8c1386c
ThayanaRMachado <thayanarm@hotmail.com>
2020-09-13 17:30:19 -0300

    Atual-2.2-Chain of Responsibility-Criando a calculadora de Descontos

--- testes.php
+++ testes.php
@@ -1,15 +1,24 @@
 <?php
 
 use Alura\DesignPattern\CalculadoraDeImpostos;
+use Alura\DesignPattern\CalculadoraDeDescontos;
 use Alura\DesignPattern\Impostos\{Icms, Iss};
 use Alura\DesignPattern\Orcamento;
 
 require 'vendor/autoload.php';
 
-$calculadora = new CalculadoraDeImpostos();
+/*$calculadora = new CalculadoraDeImpostos();
 
 $orcamento = new Orcamento();
 $orcamento->valor = 100;
 
 echo $calculadora->calcula($orcamento, new Icms());
-echo $calculadora->calcula($orcamento, new Iss());
+echo $calculadora->calcula($orcamento, new Iss());*/
+$calculadora = new CalculadoraDeDescontos();
+
+$orcamento = new Orcamento();
+$orcamento->valor = 200;
+//$orcamento->quantidadeItens = 7; //1º Teste
+$orcamento->quantidadeItens = 5; //2º Teste
+
+echo $calculadora->calculaDescontos($orcamento);
