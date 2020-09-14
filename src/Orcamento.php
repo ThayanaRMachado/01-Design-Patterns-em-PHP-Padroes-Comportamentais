e203b388ff070a48b8b481c51d724e46aaf8c89e
ThayanaRMachado <thayanarm@hotmail.com>
2020-09-14 17:01:00 -0300

    4.2-Sate-Adicionando estados ao orçamento

--- src/Orcamento.php
+++ src/Orcamento.php
@@ -6,4 +6,26 @@ class Orcamento
 {
     public float $valor;
     public int $quantidadeItens;
+    public string $estadoAtual;
+
+    public function aplicaDescontoExtra()
+    {
+        $this->valor -= $this->estadoAtual->calculaDescontoExtra();
+    }
+
+    public function calculaDescontoExtra(): float
+    {
+        if ($this->estadoAtual == 'EM_APROVACAO') {
+            return $this->valor * 0.05;
+        }
+
+        if ($this->estadoAtual == 'APROVADO') {
+            return $this->valor * 0.02;
+        }
+
+        throw new \DomainException(
+            'Orçamentos reprovados e finalizados não podem receber desconto.'
+        );
+    }
+
 }
