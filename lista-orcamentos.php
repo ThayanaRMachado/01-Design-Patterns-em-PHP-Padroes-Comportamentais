88f03b07b134acefdf5bfbf67c0c914ab03260a1
ThayanaRMachado <thayanarm@hotmail.com>
2020-09-16 16:03:59 -0300

    7-4-Iterator-representando uma coleção de orçamentos

--- lista-orcamentos.php
+++ lista-orcamentos.php
@@ -3,6 +3,7 @@
 require_once 'vendor/autoload.php';
 
 use Alura\DesignPattern\Orcamento;
+use Alura\DesignPattern\ListaDeOrcamentos;
 
 $orcamento1 = new Orcamento();
 $orcamento1->quantidadeItens = 7;
@@ -20,11 +21,10 @@ $orcamento3->aprova();
 $orcamento3->finaliza();
 $orcamento3->valor = 1350;
 
-$listaOrcamentos = [
-$orcamento1,
-$orcamento2,
-$orcamento3
-];
+$listaOrcamentos = new ListaDeOrcamentos();
+$listaOrcamentos->addOrcamento($orcamento1);
+$listaOrcamentos->addOrcamento($orcamento2);
+$listaOrcamentos->addOrcamento($orcamento3);
 
 foreach ($listaOrcamentos as $orcamento) {
     echo "Valor: " . $orcamento->valor . PHP_EOL;
